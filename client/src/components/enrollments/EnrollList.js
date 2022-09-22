2122d8f98c5da028542fac1b5e0384d168913cf3
Brennen Lovato <brennenlovato@outlook.com>
2022-09-21 20:47:46 -0600

    enrollment now in context api

--- client/src/components/enrollments/EnrollList.js
+++ client/src/components/enrollments/EnrollList.js
@@ -1,16 +1,14 @@
 import { Container, ListGroup } from 'react-bootstrap';
 import EnrollShow from './EnrollShow';
 
-const EnrollList = ({ enrollments, updateEnroll, unenrolledUsers, deleteEnroll }) => (
+const EnrollList = ({ enrollments, updateEnroll, unenrolledUsers, deleteEnroll, courseId }) => (
   <Container>
     <ListGroup variant="flush">
       { enrollments.map( e => 
         <EnrollShow 
           key={e.id}
           {...e}
-          updateEnroll={updateEnroll}
-          unenrolledUsers={unenrolledUsers}
-          deleteEnroll={deleteEnroll}
+          courseId={courseId}
         />
       )}
     </ListGroup>
