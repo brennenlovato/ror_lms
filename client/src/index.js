7fe1165a012fb731ae5d1010d59a09f49f96687a
Brennen Lovato <brennenlovato@outlook.com>
2022-09-21 20:16:27 -0600

    user update and delete working

--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,18 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
+import UserProvider from './providers/UserProvider';
+import CourseProvider from './providers/CourseProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <UserProvider>
+        <CourseProvider>
+          <App />
+        </CourseProvider>
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
